809bd10275ad9c9b15ebb61e932dfcb126b5faed
ermiyas <128813243+ermi1999@users.noreply.github.com>
2023-06-13 19:52:15 +0300

    half way through

---
+++ app/components/inputs/CategoryInput.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { IconType } from "react-icons";
+
+interface CategoryInputProps {
+  icon: IconType;
+  label: string;
+  selected?: boolean;
+}
+const CategoryInput = () => {
+  return <div></div>;
+};
+
+export default CategoryInput;
